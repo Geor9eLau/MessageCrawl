364f938a72a67a9e7268be953208dbee70fe57d0
刘文豪 <wenhao_liu3@suishouji.com>
2017-09-25 15:28:34 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .Package(
             url: "https://github.com/Geor9eLau/MessageCrawl.git",
-            majorVersion: 2, minor: 0
+            majorVersion: 10, minor: 0
         )
     ]
 )
