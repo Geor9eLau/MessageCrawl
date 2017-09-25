ae1796d70a953b2bab96f50c89a210dd6edc53ac
刘文豪 <wenhao_liu3@suishouji.com>
2017-09-25 14:44:12 +0800

    no message

---
+++ Package.swift
@@ -0,0 +1,20 @@
+//
+//  Package.swift
+//  Perefect
+//
+//  Created by George on 2017/9/25.
+//  Copyright © 2017年 George. All rights reserved.
+//
+
+import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "MyCardMessageCrawl",
+    dependencies: [
+        .Package(
+            url: "https://github.com/Geor9eLau/MessageCrawl.git",
+            majorVersion: 2, minor: 0
+        )
+    ]
+)
---
+++ Source/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  
+//
+//  Created by George on 2017/9/25.
+//
+
+import Foundation
