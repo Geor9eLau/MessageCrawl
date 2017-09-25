7ea5385957143245e12f5afbdd71f3bace67339a
刘文豪 <wenhao_liu3@suishouji.com>
2017-09-25 16:51:39 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -9,10 +9,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyCardMessageCrawl",
+    name: "MessageCrawl",
     dependencies: [
         .Package(
-            url: "https://github.com/Geor9eLau/MessageCrawl.git",
-            majorVersion: 0)
+            url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+            majorVersion: 2, minor: 0)
     ]
 )
